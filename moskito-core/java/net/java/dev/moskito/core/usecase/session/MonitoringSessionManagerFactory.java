ced0566c26b15a198a7581fb9f3a82a2f2032abc
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2009-09-23 21:24:39 +0000

    checkstyle

--- moskito-core/java/net/java/dev/moskito/core/usecase/session/MonitoringSessionManagerFactory.java
+++ moskito-core/java/net/java/dev/moskito/core/usecase/session/MonitoringSessionManagerFactory.java
@@ -1,6 +1,10 @@
 package net.java.dev.moskito.core.usecase.session;
 
+/**
+ * The factory for the monitoring session manager. 
+ */
 public class MonitoringSessionManagerFactory {
+	
 	private static IMonitoringSessionManager instance = new MonitoringSessionManagerImpl();
 	
 	public static IMonitoringSessionManager getMonitoringSessionManager(){
