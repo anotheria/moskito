323784f1157006183248177a5fb143896a5ca5c3
imercuriev <imercuriev@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2010-02-22 17:11:19 +0000

    add xml storage as junit test

--- moskito-central/test/junit/net/java/dev/moskito/central/MoskitoCentralTestSuite.java
+++ moskito-central/test/junit/net/java/dev/moskito/central/MoskitoCentralTestSuite.java
@@ -1,13 +1,14 @@
 package net.java.dev.moskito.central;
 
 import net.java.dev.moskito.central.storages.JdbcTest;
+import net.java.dev.moskito.central.storages.XmlTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value=Suite.class)
-@SuiteClasses({JdbcTest.class})
+@SuiteClasses({JdbcTest.class, XmlTest.class})
 public class MoskitoCentralTestSuite {
 
 }
