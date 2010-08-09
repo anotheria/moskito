e9289eddf5f9f524374eb0299bca7ab47ad8caca
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2010-08-09 16:34:18 +0000

    added interval impl test

---
+++ test/junit/net/java/dev/moskito/core/stats/impl/IntervalImplTest.java
@@ -0,0 +1,58 @@
+package net.java.dev.moskito.core.stats.impl;
+
+import net.java.dev.moskito.core.stats.IIntervalListener;
+import net.java.dev.moskito.core.stats.Interval;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IntervalImplTest {
+	@Test public void testListener(){
+		IntervalImpl i = new IntervalImpl(1, "blub", 1000*60);
+		MyIntervalListener primary = new MyIntervalListener();
+		MyIntervalListener secondary = new MyIntervalListener();
+		
+		i.update();
+		assertEquals(0, primary.updatecount);
+		assertEquals(0, secondary.updatecount);
+		
+		i.addPrimaryIntervalListener(primary);
+		i.update();
+		assertEquals(1, primary.updatecount);
+		assertEquals(0, secondary.updatecount);
+	
+		i.addSecondaryIntervalListener(secondary);
+		i.update();
+		assertEquals(2, primary.updatecount);
+		assertEquals(1, secondary.updatecount);
+		
+		i.removePrimaryIntervalListener(primary);
+		i.update();
+		assertEquals(2, primary.updatecount);
+		assertEquals(2, secondary.updatecount);
+		
+		i.removeSecondaryIntervalListener(secondary);
+		i.update();
+		assertEquals(2, primary.updatecount);
+		assertEquals(2, secondary.updatecount);
+		
+	}
+	
+	@Test public void testBasics(){
+		IntervalImpl i = new IntervalImpl(1, "blub", 1000*60);
+		assertEquals(1, i.getId());
+		assertEquals("blub", i.getName());
+		assertEquals(60000, i.getLength());
+		assertNotNull(i.toString());
+	}
+	
+	private static class MyIntervalListener implements IIntervalListener{
+
+		int updatecount = 0;
+		
+		@Override
+		public void intervalUpdated(Interval aCaller) {
+			updatecount++;
+		}
+	}
+}
