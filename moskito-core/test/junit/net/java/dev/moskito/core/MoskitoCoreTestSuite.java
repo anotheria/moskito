a101c0c0ad077a5d3e5f723461a8cc2c674eee2a
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2010-08-09 16:34:18 +0000

    added interval impl test

--- moskito-core/test/junit/net/java/dev/moskito/core/MoskitoCoreTestSuite.java
+++ moskito-core/test/junit/net/java/dev/moskito/core/MoskitoCoreTestSuite.java
@@ -4,6 +4,7 @@ import net.java.dev.moskito.core.predefined.RequestOrientedStatsTest;
 import net.java.dev.moskito.core.predefined.ServiceStatsParallelTest;
 import net.java.dev.moskito.core.predefined.ServiceStatsSimpleTest;
 import net.java.dev.moskito.core.registry.ProducerRegistryTest;
+import net.java.dev.moskito.core.stats.impl.IntervalImplTest;
 import net.java.dev.moskito.core.stats.impl.IntervalNameParserTest;
 import net.java.dev.moskito.core.stats.impl.NegativeCurrentRequestsTest;
 
@@ -13,7 +14,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value=Suite.class)
-@SuiteClasses({ServiceStatsParallelTest.class, ServiceStatsSimpleTest.class, IntervalNameParserTest.class, RequestOrientedStatsTest.class, NegativeCurrentRequestsTest.class, ProducerRegistryTest.class, BlueprintTestWithCalc.class})
+@SuiteClasses({ServiceStatsParallelTest.class, ServiceStatsSimpleTest.class, IntervalNameParserTest.class, RequestOrientedStatsTest.class, NegativeCurrentRequestsTest.class, ProducerRegistryTest.class, BlueprintTestWithCalc.class,
+	IntervalImplTest.class})
 public class MoskitoCoreTestSuite {
 	@BeforeClass public static void init(){
 		System.setProperty("JUNITTEST", "true");
