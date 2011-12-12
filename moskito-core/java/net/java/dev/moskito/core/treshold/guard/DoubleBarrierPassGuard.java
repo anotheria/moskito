8ded91a3090105276d5d6a188d49cb2ae986f5a5
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2011-12-12 17:07:14 +0000

    cs

--- moskito-core/java/net/java/dev/moskito/core/treshold/guard/DoubleBarrierPassGuard.java
+++ moskito-core/java/net/java/dev/moskito/core/treshold/guard/DoubleBarrierPassGuard.java
@@ -2,7 +2,14 @@ package net.java.dev.moskito.core.treshold.guard;
 
 import net.java.dev.moskito.core.treshold.ThresholdStatus;
 
+/**
+ * A barrier path guard that works with double values.
+ * @author lrosenberg
+ */
 public class DoubleBarrierPassGuard extends BarrierPassGuard{
+	/**
+	 * Limit value.
+	 */
 	private double barrierValue;
 
 	public DoubleBarrierPassGuard(ThresholdStatus aTargetStatus, double targetValue, GuardedDirection aDirection){
@@ -10,13 +17,15 @@ public class DoubleBarrierPassGuard extends BarrierPassGuard{
 		barrierValue = targetValue;
 	}
 	
+	@Override
 	public String getValueAsString(){
 		return ""+barrierValue;
 	}
-	
+	@Override
 	protected Number getValueAsNumber(String aValue){
 		return Double.parseDouble(aValue);
 	}
+	@Override
 	protected Number getBarrierValueAsNumber(){
 		return barrierValue;
 	}
