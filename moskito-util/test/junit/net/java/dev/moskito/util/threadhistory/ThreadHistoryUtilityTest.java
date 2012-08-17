5df6f0ee05bc224703da82019120e46cbe874060
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2012-08-17 07:50:50 +0000

    added asserts to the test

--- moskito-util/test/junit/net/java/dev/moskito/util/threadhistory/ThreadHistoryUtilityTest.java
+++ moskito-util/test/junit/net/java/dev/moskito/util/threadhistory/ThreadHistoryUtilityTest.java
@@ -2,6 +2,7 @@ package net.java.dev.moskito.util.threadhistory;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class ThreadHistoryUtilityTest {
 	
@@ -18,6 +19,8 @@ public class ThreadHistoryUtilityTest {
 		}
 		
 		Thread.currentThread().sleep(1500);
+		
+		assertEquals(0, ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size());
 	}
 	
 	@Test public void testOnMode() throws InterruptedException{
@@ -27,12 +30,15 @@ public class ThreadHistoryUtilityTest {
 		}
 		
 		Thread.currentThread().sleep(1500);
-
-		for (int i=0; i<THREADCOUNT; i++){
+		assertTrue(ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size()>0);
+		int oldsize = ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size();
+		
+		for (int i=0; i<THREADCOUNT+1; i++){
 			new Thread(new TestThread()).start();
 		}
 		
 		Thread.currentThread().sleep(1500);
+		assertTrue(oldsize<ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size());
 	}
 
 	static class TestThread implements Runnable{
