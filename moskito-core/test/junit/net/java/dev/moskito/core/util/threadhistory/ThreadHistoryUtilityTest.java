cc490e1de564411cd056ce53ff006cb8ba62b1a0
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2012-09-10 10:24:47 +0000

    MSK-62

---
+++ moskito-core/test/junit/net/java/dev/moskito/core/util/threadhistory/ThreadHistoryUtilityTest.java
@@ -0,0 +1,53 @@
+package net.java.dev.moskito.core.util.threadhistory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ThreadHistoryUtilityTest {
+	
+	private static final int THREADCOUNT = 5;
+
+	@BeforeClass public static void setUpdate(){
+		ThreadHistoryUtility.INSTANCE.setUpdateInterval(1000L);
+	}
+	
+	@Test public void testOffMode() throws InterruptedException{
+		ThreadHistoryUtility.INSTANCE.deactivate();
+		for (int i=0; i<THREADCOUNT; i++){
+			new Thread(new TestThread()).start();
+		}
+		
+		Thread.currentThread().sleep(1500);
+		
+		assertEquals(0, ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size());
+	}
+	
+	@Test public void testOnMode() throws InterruptedException{
+		ThreadHistoryUtility.INSTANCE.activate();
+		for (int i=0; i<THREADCOUNT; i++){
+			new Thread(new TestThread()).start();
+		}
+		
+		Thread.currentThread().sleep(1500);
+		assertTrue(ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size()>0);
+		int oldsize = ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size();
+		
+		for (int i=0; i<THREADCOUNT+1; i++){
+			new Thread(new TestThread()).start();
+		}
+		
+		Thread.currentThread().sleep(1500);
+		assertTrue(oldsize<ThreadHistoryUtility.INSTANCE.getThreadHistoryEvents().size());
+	}
+
+	static class TestThread implements Runnable{
+		public void run(){
+			try{
+				Thread.sleep(1100);
+			}catch(Exception ignored){}
+		}
+	}
+}
