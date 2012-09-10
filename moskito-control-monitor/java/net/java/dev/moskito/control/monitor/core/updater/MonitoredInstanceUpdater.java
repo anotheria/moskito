2810acf1809fcb42e3ef8d5837fb07bb5672ecf8
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2012-09-10 22:23:50 +0000

    moved to incubator

--- moskito-control-monitor/java/net/java/dev/moskito/control/monitor/core/updater/MonitoredInstanceUpdater.java
+++
@@ -1,6 +0,0 @@
-package net.java.dev.moskito.control.monitor.core.updater;
-
-public interface MonitoredInstanceUpdater {
-
-	void startInstanceStatusUpdating();
-}
--- moskito-control-monitor/java/net/java/dev/moskito/control/monitor/core/updater/StatusEntry.java
+++
@@ -1,5 +0,0 @@
-package net.java.dev.moskito.control.monitor.core.updater;
-
-public class StatusEntry {
-
-}
