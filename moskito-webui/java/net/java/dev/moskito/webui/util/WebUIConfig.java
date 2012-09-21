ec48165da1114bca1e3da0f79efd597cb9bdfaaf
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2012-09-21 07:01:38 +0000

    MSK-66

--- moskito-webui/java/net/java/dev/moskito/webui/util/WebUIConfig.java
+++ moskito-webui/java/net/java/dev/moskito/webui/util/WebUIConfig.java
@@ -5,7 +5,13 @@ import org.configureme.annotations.ConfigureMe;
 
 @ConfigureMe(name="mskwebui", allfields=true)
 public class WebUIConfig {
+	/**
+	 * Width of the producerChart.
+	 */
 	private int producerChartWidth = 1200;
+	/**
+	 * Height of the producerChart.
+	 */
 	private int producerChartHeight = 600;
 
 	public int getProducerChartWidth() {
@@ -20,17 +26,24 @@ public class WebUIConfig {
 	public void setProducerChartHeight(int producerChartHeight) {
 		this.producerChartHeight = producerChartHeight;
 	}
-	
+
+	/**
+	 * Returns WebUIConfig instance.
+	 * @return
+	 */
 	public static final WebUIConfig getInstance(){
 		return WebUIConfigInstanceHolder.instance;
 	}
-	
+
+	/**
+	 * Singleton holder class.
+	 */
 	private static class WebUIConfigInstanceHolder{
 		static WebUIConfig instance = new WebUIConfig();
 		static{
 			try{
 				ConfigurationManager.INSTANCE.configure(instance);
-			}catch(Exception e){;}//ignore
+			}catch(IllegalArgumentException e){;}//ignore
 		}
 	}
 }
