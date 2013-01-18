2ce7de11f1560667410d400b0e8d4c5b2e6de672
asamoilich <asamoilich@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2013-01-18 11:54:24 +0000

    MSK-79: refactoring

--- moskito-web/java/net/anotheria/moskito/web/filters/DebugRequestFilter.java
+++ moskito-web/java/net/anotheria/moskito/web/filters/DebugRequestFilter.java
@@ -4,6 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Enumeration;
 
 /**
@@ -15,48 +16,88 @@ public class DebugRequestFilter implements Filter {
 
     private static final String PARAM_DIAGNOSTIC_DEBUG = "mskDiagnosticsDebugRequest";
 
+    private HttpServletRequest request;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+    private enum RequestParamType{
+        HEADERS("headers:"),
+        ATTRIBUTES("attributes:"),
+        PARAMETERS("parameters:"),
+        SESSION_ATTRIBUTES("sessionAttributes:");
+
+        private String value;
+
+        private RequestParamType(String value){
+            this.value = value;
+        }
+
+        public String getValue(){
+            return value;
+        }
+    }
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (!(request instanceof HttpServletRequest) || request.getParameter(PARAM_DIAGNOSTIC_DEBUG) == null){
-            chain.doFilter(request, response);
+    public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!(req instanceof HttpServletRequest) || req.getParameter(PARAM_DIAGNOSTIC_DEBUG) == null){
+            chain.doFilter(req, response);
             return;
         }
-        HttpServletRequest req = (HttpServletRequest) request;
-        System.out.println("headers:");
-        Enumeration headerNames = req.getHeaderNames();
-        while(headerNames.hasMoreElements()) {
-            String headerName = (String)headerNames.nextElement();
-            System.out.println(headerName+" = "+req.getHeader(headerName));
-        }
-        System.out.println("attributes:");
-        Enumeration attributeNames = req.getAttributeNames();
-        while(attributeNames.hasMoreElements()) {
-            String attributeName = (String)attributeNames.nextElement();
-            System.out.println(attributeName+" = "+req.getAttribute(attributeName));
-        }
-        System.out.println("parameters:");
-        Enumeration parameterNames = req.getParameterNames();
-        while(parameterNames.hasMoreElements()) {
-            String parameterName = (String)parameterNames.nextElement();
-            System.out.println(parameterName+" = "+req.getParameter(parameterName));
-        }
-        HttpSession session = req.getSession(false);
+        request = (HttpServletRequest) req;
+        sysOut(RequestParamType.HEADERS);
+        sysOut(RequestParamType.ATTRIBUTES);
+        sysOut(RequestParamType.PARAMETERS);
+        HttpSession session = getSession();
         if(session != null){
             System.out.println("Session = "+session.getId());
-            System.out.println("sessionAttributes:");
-            Enumeration sessionAttributeNames = session.getAttributeNames();
-            while(sessionAttributeNames.hasMoreElements()) {
-                String sessionAttributeName = (String)sessionAttributeNames.nextElement();
-                System.out.println(sessionAttributeName+" = "+session.getAttribute(sessionAttributeName));
-            }
+            sysOut(RequestParamType.SESSION_ATTRIBUTES);
         }
         System.out.println("Remote ip = "+req.getRemoteAddr());
         System.out.println("Server name = "+req.getServerName());
         System.out.println("Server port = "+req.getServerPort());
+    }
+
+    private void sysOut(RequestParamType type){
+        System.out.println(type.getValue());
+        Enumeration enumeration = getEnumerationByType(type);
+        while(enumeration.hasMoreElements()) {
+            String elementName = (String)enumeration.nextElement();
+            System.out.println(elementName+" = "+ getElementValueFromRequestByType(elementName, type));
+        }
+    }
+
+    private Enumeration getEnumerationByType(RequestParamType type){
+        switch (type){
+            case HEADERS:
+                return request.getHeaderNames();
+            case ATTRIBUTES:
+                return request.getAttributeNames();
+            case PARAMETERS:
+                return request.getParameterNames();
+            case SESSION_ATTRIBUTES:
+                return getSession() == null ? Collections.emptyEnumeration() : getSession().getAttributeNames();
+        }
+        return Collections.emptyEnumeration();
+    }
+
+    private Object getElementValueFromRequestByType(String elementName, RequestParamType type){
+        switch (type){
+            case HEADERS:
+                return request.getHeader(elementName);
+            case ATTRIBUTES:
+                return request.getAttribute(elementName);
+            case PARAMETERS:
+                return request.getParameter(elementName);
+            case SESSION_ATTRIBUTES:
+                return getSession() == null ? "" : getSession().getAttribute(elementName);
+        }
+        return "";
+    }
+
+    private HttpSession getSession(){
+        return request.getSession(false);
     }
 
     @Override
