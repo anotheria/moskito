23efc1c245b6cf6b8d32c4abb4e3a9f36ded7395
another <another@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2013-08-15 15:18:00 +0000

    moved central to its own project on github

--- moskito-central/startServer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -DlocalRmiRegistryPort=9249 -DskipCentralRegistry=true -cp target/moskito-central-2.2.3-SNAPSHOT-jar-with-dependencies.jar net.anotheria.moskito.central.endpoints.rmi.generated.RMIEndpointServer
