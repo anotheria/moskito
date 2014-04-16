d56ed55f6f11e5996336638f28bfb46fdcc003ab
Leon Rosenberg <leon.rosenberg@anotheria.net>
2014-04-16 14:38:36 +0200

    removed test classes../moskito-webui/java/net/anotheria/moskito/webui/testremote/

---
+++ moskito-webui/java/net/anotheria/moskito/webui/embedded/MoSKitoInspectStartException.java
@@ -0,0 +1,10 @@
+package net.anotheria.moskito.webui.embedded;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 16.04.14 11:01
+ */
+public class MoSKitoInspectStartException {
+}
