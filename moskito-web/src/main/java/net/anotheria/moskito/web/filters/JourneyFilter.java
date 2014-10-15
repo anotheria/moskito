e30d90c40a3c9f85b94d0f87dd34099234205162
Leon Rosenberg <leon.rosenberg@anotheria.net>
2014-10-16 01:09:34 +0200

    MSK-302

--- moskito-web/src/main/java/net/anotheria/moskito/web/filters/JourneyFilter.java
+++ moskito-web/src/main/java/net/anotheria/moskito/web/filters/JourneyFilter.java
@@ -29,7 +29,7 @@ public class JourneyFilter implements Filter{
 	/**
 	 * Session attribute name for session record.
 	 */
-	private static final String SA_JOURNEY_RECORD = "mskJourneyRecord";
+	/*package visible*/static final String SA_JOURNEY_RECORD = "mskJourneyRecord";
 	
 	private static final String PARAM_JOURNEY_RECORDING = "mskJourney";
 	/**
@@ -133,45 +133,3 @@ public class JourneyFilter implements Filter{
 	
 }
 
-/**
- * Helper class that is stored in the http session and contains data about currently running journey.
- */
-class JourneyRecord implements Serializable{
-	/**
-	 * SerialVersionUID.
-	 */
-	private static final long serialVersionUID = 1L;
-	/**
-	 * Name of the session.
-	 */
-	private String name;
-	/**
-	 * Number of requests so far.
-	 */
-	private AtomicInteger requestCount;
-
-	/**
-	 * Creates a new journey filter.
-	 * @param aName
-	 */
-	JourneyRecord(String aName){
-		name = aName;
-		requestCount = new AtomicInteger(0);
-	}
-	
-	public int getRequestCount(){
-		return requestCount.incrementAndGet();
-	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	@Override public String toString(){
-		return getName()+" - "+requestCount.get();
-	}
-	
-	public String getUseCaseName(){
-		return getName()+"-"+getRequestCount();
-	}
-}
---
+++ moskito-web/src/main/java/net/anotheria/moskito/web/filters/JourneyRecord.java
@@ -0,0 +1,47 @@
+package net.anotheria.moskito.web.filters;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Helper class that is stored in the http session and contains data about currently running journey.
+ */
+class JourneyRecord implements Serializable {
+	/**
+	 * SerialVersionUID.
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Name of the session.
+	 */
+	private String name;
+	/**
+	 * Number of requests so far.
+	 */
+	private AtomicInteger requestCount;
+
+	/**
+	 * Creates a new journey filter.
+	 * @param aName
+	 */
+	JourneyRecord(String aName){
+		name = aName;
+		requestCount = new AtomicInteger(0);
+	}
+
+	public int getRequestCount(){
+		return requestCount.incrementAndGet();
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	@Override public String toString(){
+		return getName()+" - "+requestCount.get();
+	}
+
+	public String getUseCaseName(){
+		return getName()+"-"+getRequestCount();
+	}
+}
