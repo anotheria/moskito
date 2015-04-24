64b0d973dbc3fcb55d78c9ef1a099eef21ec058f
Leon Rosenberg <leon.rosenberg@anotheria.net>
2015-04-25 00:41:25 +0200

    sampling api, first try

---
+++ moskito-extensions/moskito-sampling-api/src/main/java/net/anotheria/moskito/extensions/sampling/Sample.java
@@ -0,0 +1,39 @@
+package net.anotheria.moskito.extensions.sampling;
+
+import java.util.Map;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 24.04.15 18:02
+ */
+public class Sample {
+	private String producerId;
+	private String statMapperId;
+	private Map<String,String> values;
+
+	public String getProducerId() {
+		return producerId;
+	}
+
+	public void setProducerId(String producerId) {
+		this.producerId = producerId;
+	}
+
+	public String getStatMapperId() {
+		return statMapperId;
+	}
+
+	public void setStatMapperId(String statMapperId) {
+		this.statMapperId = statMapperId;
+	}
+
+	public Map<String, String> getValues() {
+		return values;
+	}
+
+	public void setValues(Map<String, String> values) {
+		this.values = values;
+	}
+}
---
+++ moskito-extensions/moskito-sampling-api/src/main/java/net/anotheria/moskito/extensions/sampling/endpoints/servlet/SamplingServlet.java
@@ -0,0 +1,66 @@
+package net.anotheria.moskito.extensions.sampling.endpoints.servlet;
+
+import net.anotheria.moskito.extensions.sampling.Sample;
+import net.anotheria.moskito.extensions.sampling.SamplingEngine;
+import net.anotheria.moskito.web.MoskitoHttpServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 24.04.15 17:55
+ */
+@WebServlet(urlPatterns = "/sampling/sample")
+public class SamplingServlet extends MoskitoHttpServlet{
+
+	public static final String PARAM_PRODUCER_ID = "id";
+	public static final String PARAM_STAT_MAPPER_ID = "mapper";
+
+	private SamplingEngine engine;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+
+		engine = SamplingEngine.getInstance();
+	}
+
+	@Override
+	protected void moskitoDoGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		String producerId = req.getParameter(PARAM_PRODUCER_ID);
+		if (producerId==null || producerId.length()==0){
+			throw new IllegalArgumentException("ProducerId parameter '"+PARAM_PRODUCER_ID+"' may not be empty.");
+		}
+		String mapperId = req.getParameter(PARAM_STAT_MAPPER_ID);
+		if (mapperId==null || mapperId.length()==0){
+			throw new IllegalArgumentException("mapperId parameter '"+PARAM_STAT_MAPPER_ID+"' may not be empty.");
+		}
+
+		HashMap<String, String> parameters = new HashMap<String, String>();
+		Enumeration<String> paramNames = req.getParameterNames();
+		while(paramNames.hasMoreElements()){
+			String pName = paramNames.nextElement();
+			if (pName.equals(PARAM_PRODUCER_ID))
+				continue;
+			if (pName.equals(PARAM_STAT_MAPPER_ID))
+				continue;
+			parameters.put(pName.toLowerCase(), req.getParameter(pName));
+		}
+		Sample sample = new Sample();
+		sample.setProducerId(producerId);
+		sample.setStatMapperId(mapperId);
+		sample.setValues(parameters);
+
+		engine.addSample(sample);
+		//don't return anything, just status ok.
+	}
+}
