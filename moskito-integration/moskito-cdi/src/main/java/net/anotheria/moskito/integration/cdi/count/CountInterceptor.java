e9d35cd8d3a824c7b981bb55baddd04e5e3732d0
Alex Osadchy <alexandr.osadchy@gmail.com>
2015-05-09 14:26:17 +0300

    MoSKio CDI integration revisited

--- moskito-integration/moskito-cdi/src/main/java/net/anotheria/moskito/integration/cdi/count/CountInterceptor.java
+++ moskito-integration/moskito-cdi/src/main/java/net/anotheria/moskito/integration/cdi/count/CountInterceptor.java
@@ -2,17 +2,14 @@ package net.anotheria.moskito.integration.cdi.count;
 
 import net.anotheria.moskito.core.counter.CounterStats;
 import net.anotheria.moskito.core.counter.CounterStatsFactory;
+import net.anotheria.moskito.core.dynamic.IOnDemandStatsFactory;
 import net.anotheria.moskito.core.dynamic.OnDemandStatsProducer;
-import net.anotheria.moskito.core.dynamic.OnDemandStatsProducerException;
-import net.anotheria.moskito.integration.cdi.ProducerFinder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.anotheria.moskito.integration.cdi.AbstractInterceptor;
 
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.AroundTimeout;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -21,14 +18,19 @@ import java.lang.reflect.Method;
  * @author Alex Osadchy
  * @author Leon Rosenberg
  */
-@Count
 @Interceptor
-public class CountInterceptor {
+@Count
+public class CountInterceptor extends AbstractInterceptor<CounterStats> {
 
-    /**
-     * {@link Logger} instance.
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(CountInterceptor.class);
+    @Override
+    protected final Class<CounterStats> getStatsClass() {
+        return CounterStats.class;
+    }
+
+    @Override
+    protected final IOnDemandStatsFactory<CounterStats> getStatsFactory() {
+        return CounterStatsFactory.DEFAULT_INSTANCE;
+    }
 
     /**
      * Around method invoke.
@@ -42,26 +44,24 @@ public class CountInterceptor {
     public Object aroundInvoke(InvocationContext ctx) throws Throwable {
         final Method method = ctx.getMethod();
 
-        final Count annotation = method.getDeclaringClass().getAnnotation(Count.class);
+        final Count annotation = getAnnotationFromContext(ctx, Count.class);
         final CountParameter parameter = method.getAnnotation(CountParameter.class);
 
         final String producerId = annotation.producerId().isEmpty() ? method.getDeclaringClass().getSimpleName() : annotation.producerId();
         final String caseName = (parameter != null && !parameter.value().isEmpty()) ? parameter.value() : method.getName();
-        final OnDemandStatsProducer<CounterStats> onDemandProducer = ProducerFinder.getProducer(producerId, annotation.category(), annotation.subsystem(), CounterStatsFactory.DEFAULT_INSTANCE);
+        final OnDemandStatsProducer onDemandProducer = getProducer(producerId, annotation.category(), annotation.subsystem());
 
-        onDemandProducer.getDefaultStats().inc();
+        if (onDemandProducer != null) {
+            final CounterStats defaultStats = getDefaultStats(onDemandProducer);
+            final CounterStats stats = getStats(onDemandProducer, caseName);
 
-        try {
-            onDemandProducer.getStats(caseName).inc();
-        } catch (OnDemandStatsProducerException e) {
-            LOGGER.info("Couldn't get stats for case : " + caseName + ", probably limit reached");
+            if (defaultStats != null)
+                defaultStats.inc();
+            if (stats != null)
+                stats.inc();
         }
 
-        try {
-            return ctx.proceed();
-        } catch (InvocationTargetException e) {
-            throw e.getCause();
-        }
+        return proceed(ctx);
     }
 
 }
--- moskito-integration/moskito-cdi/src/main/java/net/anotheria/moskito/integration/cdi/monitor/MonitorInterceptor.java
+++ moskito-integration/moskito-cdi/src/main/java/net/anotheria/moskito/integration/cdi/monitor/MonitorInterceptor.java
@@ -1,17 +1,15 @@
 package net.anotheria.moskito.integration.cdi.monitor;
 
-import net.anotheria.moskito.core.calltrace.TracingUtil;
 import net.anotheria.moskito.core.calltrace.CurrentlyTracedCall;
 import net.anotheria.moskito.core.calltrace.RunningTraceContainer;
 import net.anotheria.moskito.core.calltrace.TraceStep;
 import net.anotheria.moskito.core.calltrace.TracedCall;
+import net.anotheria.moskito.core.calltrace.TracingUtil;
+import net.anotheria.moskito.core.dynamic.IOnDemandStatsFactory;
 import net.anotheria.moskito.core.dynamic.OnDemandStatsProducer;
-import net.anotheria.moskito.core.dynamic.OnDemandStatsProducerException;
 import net.anotheria.moskito.core.predefined.ServiceStats;
 import net.anotheria.moskito.core.predefined.ServiceStatsFactory;
-import net.anotheria.moskito.integration.cdi.ProducerFinder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.anotheria.moskito.integration.cdi.AbstractInterceptor;
 
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.AroundTimeout;
@@ -21,16 +19,23 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
+ * Monitor interceptor.
+ *
  * @author Alex Osadchy
  */
 @Monitor
 @Interceptor
-public class MonitorInterceptor {
+public class MonitorInterceptor extends AbstractInterceptor<ServiceStats> {
 
-    /**
-     * {@link Logger} instance.
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(MonitorInterceptor.class);
+    @Override
+    protected final Class<ServiceStats> getStatsClass() {
+        return ServiceStats.class;
+    }
+
+    @Override
+    protected final IOnDemandStatsFactory<ServiceStats> getStatsFactory() {
+        return ServiceStatsFactory.DEFAULT_INSTANCE;
+    }
 
     /**
      *
@@ -42,30 +47,29 @@ public class MonitorInterceptor {
     @AroundInvoke
     public Object intercept(final InvocationContext ctx) throws Throwable {
         final Method method = ctx.getMethod();
-        final Monitor annotation = method.getDeclaringClass().getAnnotation(Monitor.class);
+        final Monitor annotation = getAnnotationFromContext(ctx, Monitor.class);
 
         // if producer id isn't specified by client - use class name
         final String producerId = annotation.producerId().isEmpty() ? method.getDeclaringClass().getSimpleName() : annotation.producerId();
-        final String caseName = ctx.getMethod().getName();
-        final OnDemandStatsProducer<ServiceStats> onDemandProducer = ProducerFinder.getProducer(producerId, annotation.category(), annotation.subsystem(), ServiceStatsFactory.DEFAULT_INSTANCE);
 
-        final ServiceStats defaultStats = onDemandProducer.getDefaultStats();
-        defaultStats.addRequest();
+        final OnDemandStatsProducer onDemandProducer = getProducer(producerId, annotation.category(), annotation.subsystem());
 
-        ServiceStats methodStats = null;
-        try {
-            methodStats = onDemandProducer.getStats(caseName);
-        } catch (OnDemandStatsProducerException e) {
-             LOGGER.info("Couldn't get stats for case : " + caseName + ", probably limit reached");
+        if (onDemandProducer == null) {
+            return proceed(ctx);
         }
 
-        if (methodStats != null) {
+        final ServiceStats defaultStats = getDefaultStats(onDemandProducer);
+        final ServiceStats methodStats = getStats(onDemandProducer, ctx.getMethod().getName());
+
+        if (defaultStats != null)
+            defaultStats.addRequest();
+
+        if (methodStats != null)
             methodStats.addRequest();
-        }
 
-        TracedCall aRunningTrace = RunningTraceContainer.getCurrentlyTracedCall();
+        final TracedCall aRunningTrace = RunningTraceContainer.getCurrentlyTracedCall();
         TraceStep currentStep = null;
-        CurrentlyTracedCall currentTrace = aRunningTrace.callTraced() ? (CurrentlyTracedCall) aRunningTrace : null;
+        final CurrentlyTracedCall currentTrace = aRunningTrace.callTraced() ? (CurrentlyTracedCall) aRunningTrace : null;
         if (currentTrace != null) {
             currentStep = currentTrace.startStep(TracingUtil.buildCall(method, ctx.getParameters()), onDemandProducer);
         }
@@ -77,40 +81,33 @@ public class MonitorInterceptor {
             ret = ctx.proceed();
             return ret;
         } catch (InvocationTargetException e) {
-            defaultStats.notifyError();
-
-            if (methodStats != null) {
+            if (defaultStats != null)
+                defaultStats.notifyError();
+            if (methodStats != null)
                 methodStats.notifyError();
-            }
-
-            if (currentStep != null) {
+            if (currentStep != null)
                 currentStep.setAborted();
-            }
 
             throw e.getCause();
         } catch (Throwable t) {
-            defaultStats.notifyError();
-
-            if (methodStats != null) {
+            if (defaultStats != null)
+                defaultStats.notifyError();
+            if (methodStats != null)
                 methodStats.notifyError();
-            }
-
-            if (currentStep != null) {
+            if (currentStep != null)
                 currentStep.setAborted();
-            }
+
             throw t;
         } finally {
             final long exTime = System.nanoTime() - startTime;
 
-            defaultStats.addExecutionTime(exTime);
+            if (defaultStats != null) {
+                defaultStats.addExecutionTime(exTime);
+                defaultStats.notifyRequestFinished();
+            }
 
             if (methodStats != null) {
                 methodStats.addExecutionTime(exTime);
-            }
-
-            defaultStats.notifyRequestFinished();
-
-            if (methodStats != null) {
                 methodStats.notifyRequestFinished();
             }
 
@@ -127,6 +124,5 @@ public class MonitorInterceptor {
                 currentTrace.endStep();
             }
         }
-
     }
 }
