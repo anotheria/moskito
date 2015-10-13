a023fd19fcb1d027b87f96a8d57730c230bd698d
a1234 <stetsiuk.roma@gmail.com>
2015-10-13 10:31:17 +0300

    Fixed Accumulators annotation for method,

--- moskito-aop/src/test/java/net/anotheria/moskito/aop/AccumulatesAnnotatedClass.java
+++ moskito-aop/src/test/java/net/anotheria/moskito/aop/AccumulatesAnnotatedClass.java
@@ -9,14 +9,28 @@ import net.anotheria.moskito.aop.annotation.Monitor;
  */
 @Monitor
 @Accumulates({
-        @Accumulate(valueName = "Second", intervalName = "1m"),
-        @Accumulate(valueName = "Third", intervalName = "1m")})
-
-@Accumulate(valueName = "First", intervalName = "1m")
+        @Accumulate(valueName = "Class1", intervalName = "1m"),
+        @Accumulate(valueName = "Class2", intervalName = "5m")
+})
+@Accumulate(valueName = "Class3", intervalName = "1m")
 public class AccumulatesAnnotatedClass {
 
     public void notAnnotatedFunc(){};
 
-    @Accumulate(valueName = "AnnotatedFunc", intervalName = "1m")
-    public void annotatedFunc(){};
+    @Accumulates({
+        @Accumulate(valueName = "All1", intervalName = "1m"),
+        @Accumulate(valueName = "All2", intervalName = "1m")
+    })
+    @Accumulate(valueName = "All3", intervalName = "1m")
+    public void annotatedFuncAll(){};
+
+    @Accumulate(valueName = "Single", intervalName = "1m")
+    public void annotatedFuncSingle(){};
+
+    @Accumulates({
+            @Accumulate(valueName = "Holder1", intervalName = "1m"),
+            @Accumulate(valueName = "Holder2", intervalName = "5m")
+    })
+    public void annotatedFuncHolder(){};
+
 }
