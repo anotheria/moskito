031f0d4aa401ffc86f633ed8926be2fb77b17735
esmakula <esmakula@anotheria.net>
2017-06-02 17:32:19 +0300

    https://jira.opensource.anotheria.net/projects/MSK/issues/MSK-425 support for GarbageCollector MBean : accumulators

--- moskito-core/src/main/java/net/anotheria/moskito/core/util/BuiltInGCProducer.java
+++ moskito-core/src/main/java/net/anotheria/moskito/core/util/BuiltInGCProducer.java
@@ -1,5 +1,6 @@
 package net.anotheria.moskito.core.util;
 
+import net.anotheria.moskito.core.accumulation.Accumulators;
 import net.anotheria.moskito.core.predefined.GCStats;
 import net.anotheria.moskito.core.producers.IStats;
 import net.anotheria.moskito.core.producers.IStatsProducer;
@@ -15,64 +16,70 @@ import java.util.TimerTask;
 
 /**
  * Builtin producer for values supplied by GC.
+ *
  * @author esmakula
  */
-public class BuiltInGCProducer extends AbstractBuiltInProducer implements IStatsProducer, BuiltInProducer{
-	/**
-	 * Stats container
-	 */
-	private List<IStats> iStatsList;
+public class BuiltInGCProducer extends AbstractBuiltInProducer implements IStatsProducer, BuiltInProducer {
+    /**
+     * Stats container
+     */
+    private List<IStats> iStatsList;
 
-	/**
-	 * The monitored pool.
-	 */
-	private List<GarbageCollectorMXBean> mxBeans;
+    /**
+     * The monitored pool.
+     */
+    private List<GarbageCollectorMXBean> mxBeans;
 
-	/**
-	 * Logger.
-	 */
-	private static Logger log = LoggerFactory.getLogger(BuiltInGCProducer.class);
+    /**
+     * Logger.
+     */
+    private static Logger log = LoggerFactory.getLogger(BuiltInGCProducer.class);
 
-	public BuiltInGCProducer(){
-		mxBeans = ManagementFactory.getGarbageCollectorMXBeans();
-		iStatsList = new ArrayList<>(mxBeans.size());
-		for (GarbageCollectorMXBean bean: mxBeans){
-			iStatsList.add(new GCStats(bean.getName()));
-		}
+    public BuiltInGCProducer() {
+        mxBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        iStatsList = new ArrayList<>(mxBeans.size());
+        List<String> beanNames = new ArrayList<>();
+        for (GarbageCollectorMXBean bean : mxBeans) {
+            iStatsList.add(new GCStats(bean.getName()));
+            beanNames.add(bean.getName());
+        }
 
-		BuiltinUpdater.addTask(new TimerTask() {
-			@Override
-			public void run() {
-				readMbean();
-			}});
+        BuiltinUpdater.addTask(new TimerTask() {
+            @Override
+            public void run() {
+                readMbean();
+            }
+        });
 
-		ProducerRegistryFactory.getProducerRegistryInstance().registerProducer(this);
-	}
-	
-	@Override
-	public String getCategory() {
-		return "gc";
-	}
+        ProducerRegistryFactory.getProducerRegistryInstance().registerProducer(this);
+        Accumulators.createGCAccumulators(beanNames);
+    }
 
-	@Override
-	public String getProducerId() {
-		return "GC";
-	}
+    @Override
+    public String getCategory() {
+        return "gc";
+    }
 
-	@Override
-	public List<IStats> getStats() {
-		return iStatsList;
-	}
+    @Override
+    public String getProducerId() {
+        return "GC";
+    }
 
-	/**
-	 * Reads the management bean and extracts monitored data on regular base.
-	 */
-	private void readMbean() {
-		int i = 0;
-		for (GarbageCollectorMXBean bean: mxBeans){
-			 GCStats stat = (GCStats) iStatsList.get(i);
-			 stat.update(bean.getCollectionCount(), bean.getCollectionTime());
-			 i++;
-		}
-	}
+    @Override
+    public List<IStats> getStats() {
+        return iStatsList;
+    }
+
+    /**
+     * Reads the management bean and extracts monitored data on regular base.
+     */
+    private void readMbean() {
+        int i = 0;
+        for (GarbageCollectorMXBean bean : mxBeans) {
+            GCStats stat = (GCStats) iStatsList.get(i);
+            stat.update(bean.getCollectionCount(), bean.getCollectionTime());
+            i++;
+        }
+    }
+
 }
