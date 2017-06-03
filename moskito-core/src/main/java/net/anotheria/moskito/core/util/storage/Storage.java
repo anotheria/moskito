7196526bd31b69f7f9058200e3b2175de9f4ab87
Leon Rosenberg <leon.rosenberg@anotheria.net>
2017-06-03 23:58:22 +0200

    checkstyle warnings

--- moskito-core/src/main/java/net/anotheria/moskito/core/util/storage/Storage.java
+++ moskito-core/src/main/java/net/anotheria/moskito/core/util/storage/Storage.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
