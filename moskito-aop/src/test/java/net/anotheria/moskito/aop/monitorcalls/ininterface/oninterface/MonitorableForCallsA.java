f2c328abd7598990868e2dbd79b4c343fda3cbbc
Leon Rosenberg <leon.rosenberg@anotheria.net>
2017-06-16 13:59:52 +0200

    removed sysouts from test

--- moskito-aop/src/test/java/net/anotheria/moskito/aop/monitorcalls/ininterface/oninterface/MonitorableForCallsA.java
+++ moskito-aop/src/test/java/net/anotheria/moskito/aop/monitorcalls/ininterface/oninterface/MonitorableForCallsA.java
@@ -3,6 +3,6 @@ package net.anotheria.moskito.aop.monitorcalls.ininterface.oninterface;
 public class MonitorableForCallsA implements MonitorableForCalls {
     @Override
     public void monitored() {
-        System.out.println("MonitorableForCallsA");
+        
     }
 }
