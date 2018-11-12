880e1f21db302bc175634739d37678799136b0bd
Leon Rosenberg <leon.rosenberg@anotheria.net>
2018-11-12 21:55:15 +0100

    remove unneeded variable

--- moskito-webui/src/main/java/net/anotheria/moskito/webui/dashboards/action/ShowDashboardAction.java
+++ moskito-webui/src/main/java/net/anotheria/moskito/webui/dashboards/action/ShowDashboardAction.java
@@ -20,12 +20,15 @@ import net.anotheria.moskito.webui.shared.bean.ProducerDecoratorBean;
 import net.anotheria.moskito.webui.threshold.bean.ThresholdStatusBean;
 import net.anotheria.moskito.webui.util.WebUIConfig;
 import net.anotheria.util.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static net.anotheria.moskito.webui.threshold.util.ThresholdStatusBeanUtility.getThresholdBeans;
@@ -37,11 +40,6 @@ import static net.anotheria.moskito.webui.threshold.util.ThresholdStatusBeanUtil
  * @since 12.02.15 14:02
  */
 public class ShowDashboardAction extends BaseDashboardAction {
-
-	/**
-	 * Logger.
-	 */
-	private static final Logger log = LoggerFactory.getLogger(ShowDashboardAction.class);
 
 	/**
 	 * Default dashboard refresh rate in ms.
