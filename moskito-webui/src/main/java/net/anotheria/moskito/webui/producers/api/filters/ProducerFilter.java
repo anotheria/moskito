4b264577c96059031f36a05be06c86b8f6e36a3c
Leon Rosenberg <leon.rosenberg@anotheria.net>
2018-11-13 12:06:39 +0100

    javadoc

--- moskito-webui/src/main/java/net/anotheria/moskito/webui/producers/api/filters/ProducerFilter.java
+++ moskito-webui/src/main/java/net/anotheria/moskito/webui/producers/api/filters/ProducerFilter.java
@@ -9,7 +9,16 @@ import net.anotheria.moskito.core.producers.IStatsProducer;
  * @since 19.04.15 23:50
  */
 public interface ProducerFilter {
+	/**
+	 * If this method retuns true the producer is not filtered out.
+	 * @param statsProducer
+	 * @return
+	 */
 	boolean mayPass(IStatsProducer<?> statsProducer);
 
+	/**
+	 * Allows to customize filter after creation.
+	 * @param parameter
+	 */
 	void customize(String parameter);
 }
