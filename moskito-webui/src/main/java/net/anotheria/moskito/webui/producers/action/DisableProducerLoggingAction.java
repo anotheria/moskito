81923b2039bc70b7adfeb880aa2a12fe0b95aa25
Leon Rosenberg <leon.rosenberg@anotheria.net>
2019-09-17 16:03:46 +0200

    logging: turn on/off logging

---
+++ moskito-webui/src/main/java/net/anotheria/moskito/webui/producers/action/DisableProducerLoggingAction.java
@@ -0,0 +1,37 @@
+package net.anotheria.moskito.webui.producers.action;
+
+import net.anotheria.maf.action.ActionCommand;
+import net.anotheria.maf.action.ActionMapping;
+import net.anotheria.maf.bean.FormBean;
+import net.anotheria.moskito.webui.shared.action.BaseMoskitoUIAction;
+import net.anotheria.moskito.webui.shared.bean.NaviItem;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 2019-08-20 12:02
+ */
+public class DisableProducerLoggingAction extends BaseMoskitoUIAction {
+	@Override
+	public ActionCommand execute(ActionMapping actionMapping, FormBean formBean, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+		String producerId = getProducerIdParameter(httpServletRequest);
+		getProducerAPI().disableLogging(producerId);
+		httpServletResponse.sendRedirect("mskShowProducer?"+PARAM_PRODUCER_ID+ '=' + URLEncoder.encode(producerId, "UTF-8"));
+		return null;
+	}
+
+	@Override
+	protected String getLinkToCurrentPage(HttpServletRequest req) {
+		return null;
+	}
+
+	@Override
+	protected NaviItem getCurrentNaviItem() {
+		return null;
+	}
+}
