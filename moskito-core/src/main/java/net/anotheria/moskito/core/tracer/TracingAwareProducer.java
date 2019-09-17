52e56df8505c872f88628c06ddd1977bf713197f
Leon Rosenberg <leon.rosenberg@anotheria.net>
2019-09-17 16:06:03 +0200

    javadoc

--- moskito-core/src/main/java/net/anotheria/moskito/core/tracer/TracingAwareProducer.java
+++ moskito-core/src/main/java/net/anotheria/moskito/core/tracer/TracingAwareProducer.java
@@ -1,11 +1,15 @@
 package net.anotheria.moskito.core.tracer;
 
 /**
- * This interface should be implemented
+ * This interface should be implemented by a producer that supports tracing.
  *
  * @author lrosenberg
  * @since 05.05.15 15:27
  */
 public interface TracingAwareProducer {
+	/**
+	 * Returns true if tracing is supported by this producer.
+	 * @return
+	 */
 	boolean tracingSupported();
 }
