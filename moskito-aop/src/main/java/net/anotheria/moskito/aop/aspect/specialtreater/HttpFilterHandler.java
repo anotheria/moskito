e41c288203fa1bcb4906d5e93cf5f552085866d1
Leon Rosenberg <leon.rosenberg@anotheria.net>
2020-12-09 00:25:05 +0100

    added method to call description

--- moskito-aop/src/main/java/net/anotheria/moskito/aop/aspect/specialtreater/HttpFilterHandler.java
+++ moskito-aop/src/main/java/net/anotheria/moskito/aop/aspect/specialtreater/HttpFilterHandler.java
@@ -4,12 +4,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * TODO comment this class
+ * Handler to extract call information from an http filter.
+ * It retrieves the request URI (for now).
+ * Maybe parameters will be added later.
  *
  * @author lrosenberg
  * @since 08.12.20 15:32
  */
-public class HttpFilterTreater implements SpecialTreater{
+public class HttpFilterHandler implements SpecialCaseHandler{
 	@Override
 	public StringBuilder getCallDescription(Class clazz, String methodName, Object target, Object[] args) {
 
@@ -19,9 +21,11 @@ public class HttpFilterTreater implements SpecialTreater{
 			Object requestObject = args[0];
 			if (!requestClazz.isInstance(requestObject))
 				return null;
-			Method m = requestClazz.getMethod("getRequestURI", null);
-			return new StringBuilder().append(m.invoke(requestObject));
+			Method m2 = requestClazz.getMethod("getRequestURI", null);
+			Method m1 = requestClazz.getMethod("getMethod", null);
+			return new StringBuilder().append(m1.invoke(requestObject)).append(' ').append(m2.invoke(requestObject) );
 		}catch(ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e){
+			//TODO log handling maybe.
 			e.printStackTrace();
 			return null;
 		}
