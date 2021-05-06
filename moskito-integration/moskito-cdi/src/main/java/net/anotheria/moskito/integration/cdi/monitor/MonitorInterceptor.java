dc108aef7046e7abf39663f7ad8bf9446d808e5d
Leon Rosenberg <leon.rosenberg@anotheria.net>
2021-05-06 11:53:44 +0200

    renamed call

--- moskito-integration/moskito-cdi/src/main/java/net/anotheria/moskito/integration/cdi/monitor/MonitorInterceptor.java
+++ moskito-integration/moskito-cdi/src/main/java/net/anotheria/moskito/integration/cdi/monitor/MonitorInterceptor.java
@@ -187,7 +187,7 @@ public class MonitorInterceptor extends AbstractInterceptor<ServiceStats> implem
                 if (journeyStartedByMe) {
                     //now finish the journey.
                     Journey myJourney = JourneyManagerFactory.getJourneyManager().getOrCreateJourney(Tracers.getJourneyNameForTracers(producerId));
-                    myJourney.addUseCase((CurrentlyTracedCall) RunningTraceContainer.endTrace());
+                    myJourney.addCall((CurrentlyTracedCall) RunningTraceContainer.endTrace());
                     RunningTraceContainer.cleanup();
                 }
 
