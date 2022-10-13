863b8a505705ed70ac0853de5eb5b96bde9c15fb
Leon Rosenberg <leon@anotheria.net>
2022-10-13 13:01:42 +0200

    attempt to fix test (or see whats wrong with it)

--- moskito-webui/src/test/java/net/anotheria/moskito/webui/accumulators/api/getAccumulatorGraphDataTest.java
+++ moskito-webui/src/test/java/net/anotheria/moskito/webui/accumulators/api/getAccumulatorGraphDataTest.java
@@ -79,7 +79,8 @@ public class getAccumulatorGraphDataTest {
         assertEquals(aRepository.getAccumulators().size(), 0);
         createFakeAccumulators(new String[]{"a1", "a2", "a3", "a4", "a5", "a6", "a7"});
         assertEquals(aRepository.getAccumulators().size(), 7);
-        assertEquals(registry.getProducers().size(), 6);
+        System.out.println(registry.getProducers());
+        assertEquals("Expected 6: "+registry.getProducers(), 6, registry.getProducers().size());
 
 
         ThresholdRepository tRepository = ThresholdRepository.getInstance();
