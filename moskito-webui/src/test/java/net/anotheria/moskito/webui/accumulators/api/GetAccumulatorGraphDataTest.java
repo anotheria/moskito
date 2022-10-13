18a76c0845115e148bbb7c25b1103274ec1315a0
Leon Rosenberg <leon@anotheria.net>
2022-10-13 13:09:50 +0200

    fixed class name

--- moskito-webui/src/test/java/net/anotheria/moskito/webui/accumulators/api/GetAccumulatorGraphDataTest.java
+++ moskito-webui/src/test/java/net/anotheria/moskito/webui/accumulators/api/GetAccumulatorGraphDataTest.java
@@ -31,7 +31,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class getAccumulatorGraphDataTest {
+public class GetAccumulatorGraphDataTest {
 
     @BeforeClass
     public static void startUpTest() {
