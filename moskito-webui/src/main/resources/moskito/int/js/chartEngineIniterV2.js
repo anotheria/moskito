e6400cc69cb4c7714294850f5a859495548483f5
Yuriy Koval <yuriy.koval@phenompeople.com>
2023-08-23 18:21:17 +0300

    - use chart instance properly;  TODO: map cher data; investigate chart data update mechanism;

--- moskito-webui/src/main/resources/moskito/int/js/chartEngineIniterV2.js
+++ moskito-webui/src/main/resources/moskito/int/js/chartEngineIniterV2.js
@@ -1,20 +1,90 @@
 var chartEngineIniterV2 = {
     init: function (params) {
-        const $container = $('#' + params.container);
-        $container.empty();
-        const ctx = $('<canvas></canvas>').appendTo($container)[0].getContext('2d');
-        //var d3Chart = D3chart.getInstance();
-        //d3Chart("#" + params.container, params);
-        window.myGauge = new Chart(ctx, config);
+
+        $('#' + params.container).empty();
+
+        const d3ChartV2 = D3chartV2.getInstance();
+        d3ChartV2("#" + params.container, params);
+
     }
 };
 
-var D3chart = (function () {
+var D3chartV2 = (function () {
     var instance;
 
     var gaugeChart = function () {
         var _gauges = {};
         function Gauge(placeholderSelector, configuration) {
+
+            var randomScalingFactor = function () {
+                return Math.round(Math.random() * 100);
+            };
+
+            var randomData = function () {
+                return [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor()
+                ];
+            };
+
+            var randomValue = function (data) {
+                return Math.max.apply(null, data) * Math.random();
+            };
+
+            var data = randomData();
+            var value = randomValue(data);
+
+            var zoneColours = {
+                "green": "#109618",
+                "yellow": "#FFFF00",
+                "orange": "#FF9900",
+                "red": "#DC3912"
+            };
+
+            var config = {
+                type: 'gauge',
+                data: {
+                    labels: ['Success', 'Warning', 'Warning', 'Error'],
+                    datasets: [{
+                        data: data,
+                        value: value,
+                        backgroundColor: [zoneColours.green, zoneColours.yellow, zoneColours.orange, zoneColours.red],
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Gauge chart'
+                    },
+                    layout: {
+                        padding: {
+                            bottom: 30
+                        }
+                    },
+                    needle: {
+                        // Needle circle radius as the percentage of the chart area width
+                        radiusPercentage: 2,
+                        // Needle width as the percentage of the chart area width
+                        widthPercentage: 3.2,
+                        // Needle length as the percentage of the interval between inner radius (0%) and outer radius (100%) of the arc
+                        lengthPercentage: 80,
+                        // The color of the needle
+                        color: 'rgba(0, 0, 240, 1)'
+                    },
+                    valueLabel: {
+                        formatter: Math.round
+                    }
+                }
+            };
+
+            const $canvas = $('<canvas></canvas>').attr('id', configuration.id);
+            $(placeholderSelector).replaceWith($canvas);
+            const ctx = $(placeholderSelector)[0].getContext('2d');
+            this.chart = new Chart(ctx, config);
 
         }
 
@@ -38,6 +108,7 @@ var D3chart = (function () {
             var d = params.data;
 
             var config = {
+                id: params.container,
                 complete: d.complete,
                 size: 160,
                 label: d.name,
@@ -51,18 +122,6 @@ var D3chart = (function () {
 
             _createGauge(container, config);
 
-            dispatch.on("refreshGauge", function (params) {
-                var containerId = params.containerId,
-                    min = params.min,
-                    max = params.max,
-                    current = params.current;
-
-                if (!_isMinMaxValid(min, max)) {
-                    return;
-                }
-
-                _gauges[containerId].chart.redrawValues(min, max, current);
-            });
         })(arguments[0], arguments[1]);
     }
 
@@ -100,62 +159,4 @@ var D3chart = (function () {
         }
     };
 })();
-
-var randomScalingFactor = function () {
-    return Math.round(Math.random() * 100);
-};
-
-var randomData = function () {
-    return [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-    ];
-};
-
-var randomValue = function (data) {
-    return Math.max.apply(null, data) * Math.random();
-};
-
-var data = randomData();
-var value = randomValue(data);
-
-var config = {
-    type: 'gauge',
-    data: {
-        labels: ['Success', 'Warning', 'Warning', 'Error'],
-        datasets: [{
-            data: data,
-            value: value,
-            backgroundColor: ['green', 'yellow', 'orange', 'red'],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        responsive: true,
-        title: {
-            display: true,
-            text: 'Gauge chart'
-        },
-        layout: {
-            padding: {
-                bottom: 30
-            }
-        },
-        needle: {
-            // Needle circle radius as the percentage of the chart area width
-            radiusPercentage: 2,
-            // Needle width as the percentage of the chart area width
-            widthPercentage: 3.2,
-            // Needle length as the percentage of the interval between inner radius (0%) and outer radius (100%) of the arc
-            lengthPercentage: 80,
-            // The color of the needle
-            color: 'rgba(0, 0, 240, 1)'
-        },
-        valueLabel: {
-            formatter: Math.round
-        }
-    }
-};
 
